fc254218f2998c16aea86bc7ff2b622e023a08d6
tema2 <work.aab.25122001@gmail.com>
2024-02-13 14:07:31 +0100

    task1.9

--- 1_concepts/1_9_phantom/src/main.rs
+++ 1_concepts/1_9_phantom/src/main.rs
@@ -1,3 +1,36 @@
+use std::{marker::PhantomData, ops::RangeBounds};
+
+
+struct Fact<T> {
+    _ph: PhantomData<T>
+}
+
+impl<T> Fact<T> {
+    fn new() -> Self {
+        Self {
+            _ph: PhantomData
+        }
+    }
+}
+
+impl Fact<Vec<u64>> {
+    fn fact(&self) -> &'static str {
+        let r = rand::random::<usize>();
+        let facts = [
+            "Vec is heap-allocated",
+            "Vec may re-allocate on growing",
+            "Vec is best collection EVAR!!!!1!"
+        ];
+        facts[r % facts.len()]
+    }
+}
+
+
 fn main() {
-    println!("Implement me!");
+    let f: Fact<Vec<u64>> = Fact::new();
+    println!("Fact about Vec: {}", f.fact());
+    println!("Fact about Vec: {}", f.fact());
+    println!("Fact about Vec: {}", f.fact());
+    println!("Fact about Vec: {}", f.fact());
+    println!("Fact about Vec: {}", f.fact());
 }
