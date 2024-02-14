7bf2d8a839a2c68fe28b63c67c6d0056f6530c39
tema2 <work.aab.25122001@gmail.com>
2024-02-14 14:38:41 +0100

    Revert "task2.5 with quirk"
    This reverts commit 19eb4f1d1cb599ad9fc8c479b0d14ed42fbdeeb4.

--- 2_idioms/2_5_exhaustivity/src/lib.rs
+++ 2_idioms/2_5_exhaustivity/src/lib.rs
@@ -2,37 +2,11 @@ pub trait EventSourced<Ev: ?Sized> {
     fn apply(&mut self, event: &Ev);
 }
 
-/// inversion of EventSourced trait
-/// Usage: whenever we get an Event that our `crate::user::User` can consume
-/// we turn it into `Box<dyn ApplyEv<crate::user::User>>` and throw in `.apply(_)` call
-/// Cost: allocation, but I suspect that in closed world assumption devirtualization can kick in
-/// + arena allocators can handle if not
-pub trait ApplyEv<T>
-{
-    fn apply(&self,to: &mut T);
-}
-
-/// the core impl
-impl<T,Ev> ApplyEv<T> for Ev where T: EventSourced<Self> {
-    fn apply(&self,to: &mut T) {
-        EventSourced::apply(to, self)
-    }
-}
-
-/// the dyn impl
-impl<T> EventSourced<&dyn ApplyEv<T>> for T {
-    fn apply(&mut self, event: &&dyn ApplyEv<T>) {
-        event.apply(self)
-    }
-}
-
 pub mod user {
     use std::time::SystemTime;
 
     use super::{event, EventSourced};
 
-
-    /// this must have been a user repo
     #[derive(Debug)]
     pub struct User {
         pub id: Id,
@@ -43,52 +17,71 @@ pub mod user {
         pub deleted_at: Option<DeletionDateTime>,
     }
 
-    // there go per event impls
-
     impl EventSourced<event::UserCreated> for User {
         fn apply(&mut self, ev: &event::UserCreated) {
-            let event::UserCreated { user_id, at } = ev;
-            self.id = *user_id;
-            self.created_at = *at;
-            self.last_activity_at = LastActivityDateTime(at.0);
+            self.id = ev.user_id;
+            self.created_at = ev.at;
+            self.last_activity_at = LastActivityDateTime(ev.at.0);
         }
     }
 
     impl EventSourced<event::UserNameUpdated> for User {
         fn apply(&mut self, ev: &event::UserNameUpdated) {
-            let event::UserNameUpdated {
-                name,
-                user_id,
-                at,
-            } = ev;
-            self.name = name.clone();
+            self.name = ev.name.clone();
         }
     }
 
     impl EventSourced<event::UserBecameOnline> for User {
         fn apply(&mut self, ev: &event::UserBecameOnline) {
-            let event::UserBecameOnline { user_id, at } = ev;
-            self.online_since = Some(*at);
+            self.online_since = Some(ev.at);
         }
     }
 
     impl EventSourced<event::UserBecameOffline> for User {
         fn apply(&mut self, ev: &event::UserBecameOffline) {
-            let event::UserBecameOffline { user_id, at } = ev;
             self.online_since = None;
-            self.last_activity_at = LastActivityDateTime(*at);
+            self.last_activity_at = LastActivityDateTime(ev.at);
         }
     }
 
     impl EventSourced<event::UserDeleted> for User {
         fn apply(&mut self, ev: &event::UserDeleted) {
-            let event::UserDeleted { user_id, at } = ev;
-            self.deleted_at = Some(*at);
-            self.last_activity_at = LastActivityDateTime(at.0);
+            self.deleted_at = Some(ev.at);
+            self.last_activity_at = LastActivityDateTime(ev.at.0);
         }
     }
 
-    // fields    
+    #[derive(Debug)]
+    pub enum Event {
+        Created(event::UserCreated),
+        NameUpdated(event::UserNameUpdated),
+        Online(event::UserBecameOnline),
+        Offline(event::UserBecameOffline),
+        Deleted(event::UserDeleted),
+    }
+
+    impl EventSourced<Event> for User {
+        fn apply(&mut self, ev: &Event) {
+            // Creation
+            if let Event::Created(ev) = ev {
+                self.apply(ev);
+                return;
+            }
+            // Online/Offline
+            if let Event::Online(ev) = ev {
+                self.apply(ev);
+                return;
+            }
+            if let Event::Offline(ev) = ev {
+                self.apply(ev);
+                return;
+            }
+            // Deletion
+            if let Event::Deleted(ev) = ev {
+                self.apply(ev);
+            }
+        }
+    }
 
     #[derive(Clone, Copy, Debug)]
     pub struct Id(pub u64);
@@ -107,13 +100,6 @@ pub mod user {
 }
 
 pub mod event {
-
-    //! So far, whenever we create a new Event, all the service subs who can consume it
-    //! just implement the `EvenSourced` trait; then dispatch code in the 
-    //! event consumption module gets comp time error that they cannot send the event to a service (during casting into a `Box<dyn ApplyEv<_>>`)
-    //! 
-    //! Or, we could slap `#[non_exhaustive]` on that event enum... =)
-
     use std::time::SystemTime;
 
     use super::user;
@@ -148,7 +134,4 @@ pub mod event {
         pub user_id: user::Id,
         pub at: user::DeletionDateTime,
     }
-
-
-
 }
