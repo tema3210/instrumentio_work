29e8523888161749a4268cb2a88c5833e9d22610
tema2 <work.aab.25122001@gmail.com>
2024-02-15 10:09:33 +0100

    task2.6

--- 2_idioms/2_6_sealing/src/lib.rs
+++ 2_idioms/2_6_sealing/src/lib.rs
@@ -2,3 +2,13 @@ pub mod my_error;
 pub mod my_iterator_ext;
 
 pub use self::{my_error::MyError, my_iterator_ext::MyIteratorExt};
+
+// cannot bc trait is sealed
+// impl<I,F> MyIteratorExt for std::iter::Map<I,F> {
+
+// }
+
+// cannot call a sealed method
+fn run_me() {
+    //MyError::type_id("a static ref", ??)
+}
