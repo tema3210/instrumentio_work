4a81306ab4acdd79984cde7da2b72be8b6aab4c1
tema2 <work.aab.25122001@gmail.com>
2024-02-19 10:16:16 +0100

    fmt

--- 1_concepts/1_2_box_pin/src/main.rs
+++ 1_concepts/1_2_box_pin/src/main.rs
@@ -5,26 +5,35 @@ fn main() {
     SayHi::say_hi(Pin::new(&&arr[..]));
 }
 
-// `Box<T>`, `Rc<T>`, `Vec<T>`, `String`, `&[u8]`, `T`.  
+// `Box<T>`, `Rc<T>`, `Vec<T>`, `String`, `&[u8]`, `T`.
 trait SayHi: fmt::Debug {
     fn say_hi(self: Pin<&Self>) {
         println!("Hi from {:?}", self)
     }
 }
 
-impl<T> SayHi for Box<T> where T: fmt::Debug {
+impl<T> SayHi for Box<T>
+where
+    T: fmt::Debug,
+{
     fn say_hi(self: Pin<&Self>) {
         println!("Hi from {:?}", self)
     }
 }
 
-impl<T> SayHi for Rc<T> where T: fmt::Debug {
+impl<T> SayHi for Rc<T>
+where
+    T: fmt::Debug,
+{
     fn say_hi(self: Pin<&Self>) {
         println!("Hi from {:?}", self)
     }
 }
 
-impl<T> SayHi for Vec<T> where T: fmt::Debug {
+impl<T> SayHi for Vec<T>
+where
+    T: fmt::Debug,
+{
     fn say_hi(self: Pin<&Self>) {
         println!("Hi from {:?}", self)
     }
@@ -55,7 +64,7 @@ trait MutMeSomehow {
     /// obviously call something requiring `&mut self`.
     /// The point here is to practice dealing with
     /// `Pin<&mut Self>` -> `&mut self` conversion
-    /// in different contexts, without introducing 
+    /// in different contexts, without introducing
     /// any `Unpin` trait bounds.
     fn mut_me_somehow(self: Pin<&mut Self>);
 }
@@ -63,20 +72,19 @@ trait MutMeSomehow {
 /// shoot in leg
 /// DO NOT USE EVER
 fn get_a_menaingful_T<T>() -> T {
-    unsafe { std::mem::zeroed()}
+    unsafe { std::mem::zeroed() }
 }
 
 /// Same thing
 
 // impl<T> MutMeSomehow for T {
 //     fn mut_me_somehow(mut self: Pin<&mut Self>) {
-        
+
 //         self.set(get_a_menaingful_T)
 //     }
 // }
 
-impl<T> MutMeSomehow for Box<T>  {
-
+impl<T> MutMeSomehow for Box<T> {
     fn mut_me_somehow(mut self: Pin<&mut Self>) {
         self.set(get_a_menaingful_T())
     }
@@ -103,10 +111,9 @@ impl MutMeSomehow for String {
 
 impl MutMeSomehow for &[u8] {
     fn mut_me_somehow(mut self: Pin<&mut Self>) {
-        self.set(&self[1..]) //cut the leftmost slice element 
+        self.set(&self[1..]) //cut the leftmost slice element
     }
 }
-
 
 //###################################################
 
@@ -116,32 +123,38 @@ struct MeasurableFuture<Fut> {
 }
 
 impl<F> MeasurableFuture<F> {
-    fn project<'s>(self: Pin<&'s mut Self>) -> (Pin<&'s mut F>,&'s mut Option<std::time::Instant>) {
-        
+    fn project<'s>(
+        self: Pin<&'s mut Self>,
+    ) -> (Pin<&'s mut F>, &'s mut Option<std::time::Instant>) {
         // Safety: we don't move anything here
         let this = unsafe { self.get_unchecked_mut() };
-        
+
         // safety: and here also
-        let inner = unsafe { Pin::new_unchecked(&mut this.inner_future)};
+        let inner = unsafe { Pin::new_unchecked(&mut this.inner_future) };
         let time_ = &mut this.started_at;
 
         //no intersection between pointers, and pin is preserved
-        (inner,time_)
+        (inner, time_)
     }
 }
 
-impl<Fut> Future for MeasurableFuture<Fut> where Fut: Future {
+impl<Fut> Future for MeasurableFuture<Fut>
+where
+    Fut: Future,
+{
     type Output = Fut::Output;
 
-    fn poll(self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
+    fn poll(
+        self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Self::Output> {
+        let (inner, started_at) = self.project();
 
-        let (inner,started_at) = self.project();
-
-        match Future::poll(inner,cx) {
+        match Future::poll(inner, cx) {
             std::task::Poll::Ready(out) => {
-                println!("time elapsed: {:?}",started_at.unwrap().elapsed());
+                println!("time elapsed: {:?}", started_at.unwrap().elapsed());
                 std::task::Poll::Ready(out)
-            },
+            }
             std::task::Poll::Pending => std::task::Poll::Pending,
         }
     }
