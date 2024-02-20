34f5c6120e8e5bab729612028240d2113095b473
tema2 <work.aab.25122001@gmail.com>
2024-02-20 13:58:08 +0100

    stubs

--- 3_ecosystem/3_7_rand_crypto/src/main.rs
+++ 3_ecosystem/3_7_rand_crypto/src/main.rs
@@ -1,3 +1,27 @@
+use std::path::PathBuf;
+
+fn select_rand_val<T>(slice: &[T]) -> &T {
+    &slice[rand::random::<usize>() % slice.len()]
+}
+
+fn get_file_hash(path: PathBuf) -> String{
+    unimplemented!();
+}
+
+fn hash_password<S: AsRef<str>>(pass: S) -> String {
+    unimplemented!();
+}
+
+// newtype 'd be better
+fn new_access_token() -> String {
+    unimplemented!();
+}
+
+fn generate_password() -> String {
+    unimplemented!()
+}
+
+
 fn main() {
     println!("Implement me!");
 }
