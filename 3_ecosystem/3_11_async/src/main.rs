1506f21fa67ad7e7cb5866305f71c43cc8202fd6
tema2 <work.aab.25122001@gmail.com>
2024-02-22 14:25:04 +0100

    initial

--- 3_ecosystem/3_11_async/src/main.rs
+++ 3_ecosystem/3_11_async/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
+use std::path::PathBuf;
+use clap::Parser;
+
+/// util to download web pages
+#[derive(Parser)]
+#[command(version, about, long_about = None, author)]
+struct Args {
+    /// maximum thread usage
+    max_threads: Option<u16>
+    
+    file: PathBuf
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse()
+
+
+
     println!("Implement me!");
 }
