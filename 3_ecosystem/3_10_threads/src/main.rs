99927af0831cc0a1ef84b9ad59519d27aa160c27
tema2 <work.aab.25122001@gmail.com>
2024-02-22 14:05:30 +0100

    fmt

--- 3_ecosystem/3_10_threads/src/main.rs
+++ 3_ecosystem/3_10_threads/src/main.rs
@@ -4,19 +4,18 @@ use std::simd::u32x64;
 use crossbeam_channel::{unbounded, Receiver, Sender};
 use rand::Rng;
 
-
 type Element = u32;
 
 /// i'll assume that 4096 is total matrix size, not per side
 // this gives 64 els per side
-type Matrix = [[Element;64];64];
+type Matrix = [[Element; 64]; 64];
 
-type Simd = std::simd::Simd<Element,64>;
+type Simd = std::simd::Simd<Element, 64>;
 
 const RNG_N_LIMIT: Element = 500;
 
 fn producer(sender: Sender<Matrix>, limit: u8) {
-    let mut mat = [[0;64];64]; //4 KiB memory wasted
+    let mut mat = [[0; 64]; 64]; //4 KiB memory wasted
 
     let mut rng = rand::thread_rng();
     for _ in 0..limit {
@@ -28,11 +27,10 @@ fn producer(sender: Sender<Matrix>, limit: u8) {
         }
         //send
         sender.send(mat).unwrap();
-    };
+    }
 }
 
-fn consumer(receiver: Receiver<Matrix>,name: String) {
-
+fn consumer(receiver: Receiver<Matrix>, name: String) {
     fn fast_sum(x: &Matrix) -> Element {
         let mut sum = Simd::splat(0);
         for i in x {
@@ -40,12 +38,11 @@ fn consumer(receiver: Receiver<Matrix>,name: String) {
         }
         sum.as_array().iter().sum()
     }
-    
+
     for i in receiver {
-        println!("{} sums to {:#?}",&name,fast_sum(&i));
+        println!("{} sums to {:#?}", &name, fast_sum(&i));
     }
 }
-
 
 fn main() {
     std::thread::scope(|s| {
@@ -53,11 +50,11 @@ fn main() {
         s.spawn(|| producer(snd, 32));
         {
             let rcv = rcv.clone();
-            s.spawn(|| consumer(rcv,"cons1".into()));
+            s.spawn(|| consumer(rcv, "cons1".into()));
         }
         {
             let rcv = rcv.clone();
-            s.spawn(|| consumer(rcv,"cons2".into()));
+            s.spawn(|| consumer(rcv, "cons2".into()));
         }
     });
 }
