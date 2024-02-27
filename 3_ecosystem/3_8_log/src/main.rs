2f33881b9f16338324733d2070d4662b10b7e988
tema2 <work.aab.25122001@gmail.com>
2024-02-27 13:33:26 +0100

    done + fmt

--- 3_ecosystem/3_8_log/src/main.rs
+++ 3_ecosystem/3_8_log/src/main.rs
@@ -1,18 +1,29 @@
 use std::{
-    fmt::format, fs::File, io::{BufWriter, Write}, iter::Once, mem::zeroed, path::{Path, PathBuf}, sync::Mutex
+    fs::File,
+    io::{BufWriter, Write},
+    path::Path,
+    sync::Mutex,
 };
 
 use once_cell::sync::OnceCell;
 
-struct AppLog;
+pub struct AppLog;
 
 impl AppLog {
-    fn init() {
-        log::set_logger(&AppLog).expect("cannot set logger");
+    pub fn init() {
+        static APP_LOG: AppLog = AppLog;
+
+        log::set_logger(&APP_LOG)
+            .map(|()| log::set_max_level(log::LevelFilter::Info))
+            .expect("cannot set logger");
     }
 }
 
- fn make_json_message<M: AsRef<str>>(lvl: log::Level,rec: &log::Record, msg: M) -> serde_json::Value {
+fn make_json_message<M: AsRef<str>>(
+    lvl: log::Level,
+    rec: &log::Record,
+    msg: M,
+) -> serde_json::Value {
     let value = serde_json::json!({
         "lvl": lvl.as_str(),
         "time": format!("{:?}",std::time::Instant::now()),
@@ -20,7 +31,7 @@ impl AppLog {
         "msg": msg.as_ref()
     });
     value
- }
+}
 
 impl log::Log for AppLog {
     fn enabled(&self, _: &log::Metadata) -> bool {
@@ -28,12 +39,15 @@ impl log::Log for AppLog {
     }
 
     fn log(&self, record: &log::Record) {
+        // panic!();
         if record.level() > log::Level::Warn {
-            let msg = make_json_message(record.level(), record, record.args().as_str().unwrap_or(""));
-            write!(std::io::stderr(),"{}",msg.to_string()).unwrap();
+            let msg =
+                make_json_message(record.level(), record, record.args().as_str().unwrap_or(""));
+            let _ = writeln!(std::io::stderr(), "{}", msg);
         } else {
-            let msg = make_json_message(record.level(), record, record.args().as_str().unwrap_or(""));
-            write!(std::io::stdout(),"{}",msg.to_string()).unwrap();
+            let msg =
+                make_json_message(record.level(), record, record.args().as_str().unwrap_or(""));
+            let _ = writeln!(std::io::stdout(), "{}", msg);
         }
     }
 
@@ -43,33 +57,35 @@ impl log::Log for AppLog {
     }
 }
 
-struct AccessLog(OnceCell<(Mutex<BufWriter<File>>, String)>);
+pub struct AccessLog(OnceCell<(Mutex<BufWriter<File>>, String)>);
 
 impl AccessLog {
-    fn init<P: AsRef<Path>>(to: P) {
+    pub fn init<P: AsRef<Path>>(to: P) {
         let file = File::create(to.as_ref()).unwrap();
         let fname = to.as_ref().file_name().unwrap();
-        // now mutex holds absolutely invalid instance which we cannot create
+
         static INSTANCE: AccessLog = AccessLog(OnceCell::new());
 
-        INSTANCE.0.get_or_init(|| (
-            Mutex::new(BufWriter::new(file)),
-            fname.to_str().expect("cannot name a file").to_owned()
-        ));
+        INSTANCE.0.get_or_init(|| {
+            (
+                Mutex::new(BufWriter::new(file)),
+                fname.to_str().expect("cannot name a file").to_owned(),
+            )
+        });
 
-
-        log::set_logger(&INSTANCE).expect("cannot set logger");
+        log::set_logger(&INSTANCE)
+            .map(|()| log::set_max_level(log::LevelFilter::Info))
+            .expect("cannot set logger");
     }
 
-    fn write_message<M: AsRef<str>>(&self,val: serde_json::Value) {
-        self
-            .0
+    fn write_message(&self, val: serde_json::Value) {
+        self.0
             .get()
             .expect("cannot get")
             .0
             .lock()
             .map(|mut g| {
-                writeln!(&mut g,"{}", val.to_string()).expect("cannot write the message");
+                writeln!(&mut g, "{}", val).expect("cannot write the message");
             })
             .expect("death");
     }
@@ -84,12 +100,11 @@ impl log::Log for AccessLog {
         let Some(s) = record.args().as_str() else {
             panic!("cannot get the data")
         };
-        self.write_message::<&str>(make_json_message(record.level(), record, s))
+        self.write_message(make_json_message(record.level(), record, s))
     }
 
     fn flush(&self) {
-        self
-            .0
+        self.0
             .get()
             .expect("cannot get")
             .0
@@ -109,7 +124,7 @@ mod tests {
     const PATH: &'static str = "./access.log";
 
     #[test]
-    fn test_fisrt_logger() {
+    fn test_first_logger() {
         AppLog::init();
 
         log::trace!("trace");
