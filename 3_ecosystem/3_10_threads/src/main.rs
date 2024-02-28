7e8753ba39d391982416e25c8c807758a95ccce3
tema2 <work.aab.25122001@gmail.com>
2024-02-28 12:35:04 +0100

    review

--- 3_ecosystem/3_10_threads/src/main.rs
+++ 3_ecosystem/3_10_threads/src/main.rs
@@ -1,13 +1,10 @@
 #![feature(portable_simd)]
-use std::simd::u32x64;
 
 use crossbeam_channel::{unbounded, Receiver, Sender};
 use rand::Rng;
 
 type Element = u32;
 
-/// i'll assume that 4096 is total matrix size, not per side
-// this gives 64 els per side
 type Matrix = [[Element; 64]; 64];
 
 type Simd = std::simd::Simd<Element, 64>;
@@ -15,17 +12,15 @@ type Simd = std::simd::Simd<Element, 64>;
 const RNG_N_LIMIT: Element = 500;
 
 fn producer(sender: Sender<Matrix>, limit: u8) {
-    let mut mat = [[0; 64]; 64]; //4 KiB memory wasted
+    let mut mat = [[0; 64]; 64];
 
     let mut rng = rand::thread_rng();
     for _ in 0..limit {
-        //regen
         for row in &mut mat {
             for col in &mut row[..] {
                 *col = rng.gen::<u32>() % RNG_N_LIMIT;
             }
         }
-        //send
         sender.send(mat).unwrap();
     }
 }
