e21a679f4ad8fbf0d37e704ee47c281fddb1f480
tema2 <work.aab.25122001@gmail.com>
2024-03-01 12:54:42 +0100

    fmt

--- 3_ecosystem/src/main.rs
+++ 3_ecosystem/src/main.rs
@@ -1,14 +1,16 @@
-use std::{io::BufRead, path::{Path, PathBuf}};
-use crossbeam_channel::{unbounded, Receiver};
 use clap::Parser;
+use crossbeam_channel::{unbounded, Receiver};
+use std::{
+    io::BufRead,
+    path::{Path, PathBuf},
+};
 
 /// Strips the JPEG metadata and minimizes size
-#[derive(Parser,Debug)]
+#[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-
     //dir for output files
-    #[arg(short,long)]
+    #[arg(short, long)]
     out_dir: Option<PathBuf>,
 
     /// how many images are processed at the same time
@@ -26,17 +28,16 @@ struct Args {
     #[arg(long)]
     config_file: Option<PathBuf>,
 
-    images: Option<Vec<String>>
-
+    images: Option<Vec<String>>,
 }
 
-#[derive(serde::Deserialize,Debug)]
+#[derive(serde::Deserialize, Debug)]
 struct Conf {
     out_dir: PathBuf,
     jobs: usize,
     quality: f64,
 
-    image_list: Option<PathBuf>
+    image_list: Option<PathBuf>,
 }
 
 fn main() {
@@ -46,58 +47,53 @@ fn main() {
     log::info!("App launched");
 
     let mut builder = config::Config::builder()
-        .add_source(
-            config::File::new(
-                &args.config_file.clone().unwrap_or("./config.json".into()).to_string_lossy(),
-                 config::FileFormat::Json
-            )
-        )
-        .add_source(
-            config::Environment::with_convert_case(
-                config::Case::UpperSnake
-            )
-        )
+        .add_source(config::File::new(
+            &args
+                .config_file
+                .clone()
+                .unwrap_or("./config.json".into())
+                .to_string_lossy(),
+            config::FileFormat::Json,
+        ))
+        .add_source(config::Environment::with_convert_case(
+            config::Case::UpperSnake,
+        ))
         .set_default(
-            "out_dir", 
-            args.out_dir.clone().unwrap_or("./".into()).display().to_string()
+            "out_dir",
+            args.out_dir
+                .clone()
+                .unwrap_or("./".into())
+                .display()
+                .to_string(),
         )
         .unwrap()
-        .set_default(
-            "jobs", 
-            (num_cpus::get() / 2).to_string()
-        )
+        .set_default("jobs", (num_cpus::get() / 2).to_string())
         .unwrap()
-        .set_default(
-            "quality", 
-            "100.0"
-        )
+        .set_default("quality", "100.0")
         .unwrap();
 
     if let Some(pathb) = args.images_file {
-        builder = builder.set_default(
-            "image_list",
-            pathb.display().to_string()
-        )
-        .unwrap();
+        builder = builder
+            .set_default("image_list", pathb.display().to_string())
+            .unwrap();
     }
 
     log::info!("Config read");
     let conf: Conf = builder.build().unwrap().try_deserialize().unwrap();
 
-    work(&conf,&args.images.unwrap_or(vec![]));
+    work(&conf, &args.images.unwrap_or(vec![]));
 }
-
 
 type Compression = u8;
 
 // into usize percents of quality
 fn process_quality(input: f64) -> Compression {
-    let sane  = (input % 100.0).clamp(10.0,90.0).round() as Compression;
+    let sane = (input % 100.0).clamp(10.0, 90.0).round() as Compression;
     100 - sane // complementary
-    // sane // direct
+               // sane // direct
 }
 
-fn process_image_bytes<B: AsRef<[u8]>>(bytes: B, compress: Compression) -> Result<Vec<u8>,String> {
+fn process_image_bytes<B: AsRef<[u8]>>(bytes: B, compress: Compression) -> Result<Vec<u8>, String> {
     // Decode the image
     let img = image::load_from_memory(bytes.as_ref())
         .map_err(|err| format!("Failed to decode image: {}", err))?;
@@ -107,47 +103,58 @@ fn process_image_bytes<B: AsRef<[u8]>>(bytes: B, compress: Compression) -> Resul
 
     // Encode the image with compression
     let mut compressed_data = std::io::Cursor::new(Vec::new());
-    rgba_image.write_to(&mut compressed_data, image::ImageOutputFormat::Jpeg(compress))
+    rgba_image
+        .write_to(
+            &mut compressed_data,
+            image::ImageOutputFormat::Jpeg(compress),
+        )
         .map_err(|err| format!("Failed to compress image: {}", err))?;
 
     Ok(compressed_data.into_inner())
 }
 
-fn process_file(path: &Path,cfg: &Conf) -> Result<(),String> {
+fn process_file(path: &Path, cfg: &Conf) -> Result<(), String> {
     let Some(fname) = path.file_name() else {
-        return Err(format!("supplied path is a dir: {}",path.display()))
+        return Err(format!("supplied path is a dir: {}", path.display()));
     };
 
-    let image_data = std::fs::read(path).map_err(|_| format!("cannot read file: {}",path.display()))?;
-    
+    let image_data =
+        std::fs::read(path).map_err(|_| format!("cannot read file: {}", path.display()))?;
+
     let image_data = process_image_bytes(image_data, process_quality(cfg.quality))?;
 
-    save_bytes(image_data,cfg,fname)
+    save_bytes(image_data, cfg, fname)
 }
 
-fn process_url(url: &url::Url,cfg: &Conf) -> Result<(),String> {
+fn process_url(url: &url::Url, cfg: &Conf) -> Result<(), String> {
     // Fetch the image from the URL
     let response = reqwest::blocking::get(url.as_str())
         .map_err(|err| format!("Failed to fetch image from URL: {}", err))?;
 
     // Ensure the request was successful (status code 2xx)
     if !response.status().is_success() {
-        return Err(format!("Failed to fetch image. Status code: {}", response.status()));
+        return Err(format!(
+            "Failed to fetch image. Status code: {}",
+            response.status()
+        ));
     }
 
-    let file_name = url.path_segments().and_then(|segments| segments.last()).unwrap_or(url.as_str());
+    let file_name = url
+        .path_segments()
+        .and_then(|segments| segments.last())
+        .unwrap_or(url.as_str());
 
     // Read the image data
-    let image_data = response.bytes()
+    let image_data = response
+        .bytes()
         .map_err(|err| format!("Failed to read image data: {}", err))?;
 
     let image_data = process_image_bytes(image_data, process_quality(cfg.quality))?;
 
-    save_bytes(image_data,cfg,file_name)
-    
+    save_bytes(image_data, cfg, file_name)
 }
 
-fn save_bytes<B: AsRef<[u8]>>(buff: B, cfg: &Conf, fname: impl AsRef<Path>) -> Result<(),String> {
+fn save_bytes<B: AsRef<[u8]>>(buff: B, cfg: &Conf, fname: impl AsRef<Path>) -> Result<(), String> {
     // Create the output directory if it doesn't exist
     std::fs::create_dir_all(&cfg.out_dir)
         .map_err(|err| format!("Failed to create output directory: {}", err))?;
@@ -162,25 +169,24 @@ fn save_bytes<B: AsRef<[u8]>>(buff: B, cfg: &Conf, fname: impl AsRef<Path>) -> R
     Ok(())
 }
 
-fn worker(rcv: Receiver<String>,cfg: &Conf) {
+fn worker(rcv: Receiver<String>, cfg: &Conf) {
     while let Ok(s) = rcv.recv() {
         if let Ok(url) = s.parse::<url::Url>() {
-            let res = process_url(&url,cfg);
+            let res = process_url(&url, cfg);
             if let Err(msg) = res {
-                log::error!("Error processing url {}: {}",&url,msg);
+                log::error!("Error processing url {}: {}", &url, msg);
             }
         }
         if let Ok(path) = s.parse::<PathBuf>() {
             let res = process_file(&path, cfg);
             if let Err(msg) = res {
-                log::error!("Error processing file {:?}: {}",&path,msg);
+                log::error!("Error processing file {:?}: {}", &path, msg);
             }
         }
     }
 }
 
-fn work(conf: &Conf,cli_req: &[String]) {
-    
+fn work(conf: &Conf, cli_req: &[String]) {
     let (snd, rcv) = unbounded::<String>();
     log::info!("Work queue created");
 
@@ -199,11 +205,11 @@ fn work(conf: &Conf,cli_req: &[String]) {
             log::info!("Enter image sources! q! to exit");
             loop {
                 let _ = stdin.read_line(&mut line);
-    
+
                 match &*line {
                     "q!\n" => {
                         break;
-                    },
+                    }
                     line => {
                         let _ = snd.send(line.trim().into());
                     }
@@ -213,7 +219,6 @@ fn work(conf: &Conf,cli_req: &[String]) {
         }
     };
 
-
     //read file and fill the channel with its content
     {
         let snd = snd.clone();
@@ -221,7 +226,7 @@ fn work(conf: &Conf,cli_req: &[String]) {
             if let Ok(file) = std::fs::File::open(fname) {
                 let mut reader = std::io::BufReader::new(file);
                 let mut line = String::new();
-                
+
                 loop {
                     match reader.read_line(&mut line) {
                         Ok(0) => break,
@@ -232,7 +237,6 @@ fn work(conf: &Conf,cli_req: &[String]) {
                         Err(_) => break,
                     }
                 }
-    
             }
         }
     }
@@ -243,11 +247,10 @@ fn work(conf: &Conf,cli_req: &[String]) {
         move |s| {
             for i in 0..conf.jobs {
                 let rcv = rcv.clone();
-                log::trace!("worker thread {} spawned!",i);
-                s.spawn(move || worker(rcv.clone(),conf));
-            };
+                log::trace!("worker thread {} spawned!", i);
+                s.spawn(move || worker(rcv.clone(), conf));
+            }
             consume_stdin();
         }
     });
-
 }
