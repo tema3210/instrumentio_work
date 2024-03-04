54118a343b0a0c6d707b010c0a5d7a2903b37d6d
tema2 <work.aab.25122001@gmail.com>
2024-03-04 11:01:48 +0100

    review

--- 3_ecosystem/3_7_rand_crypto/src/main.rs
+++ 3_ecosystem/3_7_rand_crypto/src/main.rs
@@ -1,28 +1,42 @@
-use std::{collections::HashSet, io::BufRead, path::PathBuf};
+use std::{io::BufRead, path::PathBuf};
 
 use argon2::{password_hash::SaltString, Argon2, PasswordHasher};
 use base64ct::Encoding;
-use rand::rngs::OsRng;
-use uuid::Uuid;
+use rand::{rngs::OsRng, seq::SliceRandom, Rng};
 
-fn generate_password(alphabet: &[char], len: u8) -> String {
-    let mut s = String::with_capacity(len as usize);
-    for _ in 0..len {
-        s.push(*select_rand_val(alphabet))
+pub fn generate_password(alphabet: &[char], len: u8) -> String {
+    let mut rng = rand::thread_rng();
+    (0..len).fold(
+        String::with_capacity(len as usize),
+        move |mut s,_| {
+            let idx = rng.gen_range(0..alphabet.len());
+            let ch = alphabet[idx];
+            s.push(ch);
+            s
+        }
+    )
+}
+
+pub fn select_rand_val<T>(slice: &[T]) -> &T {
+    slice.choose(&mut rand::thread_rng()).expect("empty slice")
+}
+
+pub fn new_access_token() -> String {
+    let mut rng = rand::thread_rng(); // it's also a CryptoRng
+    let mut res = String::with_capacity(64);
+
+    let ranges = ['0'..='9','A'..='Z','a'..='z'];
+
+    for _ in 0..64 {
+        let range = ranges.choose(&mut rng).unwrap().clone();
+        let ch = rng.gen_range(range);
+        res.push(ch);
     }
-    s.shrink_to_fit();
-    s
+
+    res
 }
 
-fn select_rand_val<T>(slice: &[T]) -> &T {
-    &slice[rand::random::<usize>() % slice.len()]
-}
-
-fn new_access_token() -> Uuid {
-    Uuid::new_v4()
-}
-
-fn get_file_hash(path: PathBuf) -> String {
+pub fn get_file_hash(path: PathBuf) -> String {
     use sha3::{Digest, Sha3_512};
 
     let mut hasher = Sha3_512::new();
@@ -49,7 +63,7 @@ fn get_file_hash(path: PathBuf) -> String {
 }
 
 //use argon2
-fn hash_password<S: AsRef<str>>(pass: S) -> Option<String> {
+pub fn hash_password<S: AsRef<str>>(pass: S) -> Option<String> {
     let argon2 = Argon2::default();
 
     let salt = SaltString::generate(&mut OsRng);
