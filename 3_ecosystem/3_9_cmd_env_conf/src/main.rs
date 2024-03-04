cde41745af9f3fb1aca698526f6c4712d3cf6b2e
tema2 <work.aab.25122001@gmail.com>
2024-03-04 13:16:42 +0100

    do it

--- 3_ecosystem/3_9_cmd_env_conf/src/main.rs
+++ 3_ecosystem/3_9_cmd_env_conf/src/main.rs
@@ -1,3 +1,4 @@
+#![feature(adt_const_params)]
 use clap::Parser;
 use config::{Config, Environment, File, FileFormat};
 
@@ -13,24 +14,116 @@ struct Opts {
     debug: bool,
 }
 
-#[derive(serde::Deserialize)]
-struct Conf {
-    conf_file: String,
-    name: String,
+mod conf {
+    /// if only they had literal support... see https://github.com/serde-rs/serde/issues/368
+
+    const fn default_bool<const V: bool>() -> bool {
+        V
+    }
+    const fn default_usize<const V: usize>() -> usize {
+        V
+    }
+    const fn default_u16<const V: u16>() -> u16 {
+        V
+    }
+
+    //makes alloc in runtime =(
+    fn default_str<const V: &'static str>() -> String {
+        String::from(V)
+    }
+
+    #[derive(serde::Deserialize,Debug)]
+    pub struct Conf {
+        db: Db,
+        mode: Mode,
+        server: Server,
+        log: Log,
+        background: Background,
+    }
+
+    #[derive(serde::Deserialize,Debug)]
+    pub struct Mode {
+        #[serde(default = "default_bool::<false>")]
+        debug: bool
+    }
+
+    #[derive(serde::Deserialize,Debug)]
+    pub struct Server {
+        #[serde(default = "default_str::<\"http://127.0.0.1\">")]
+        external_url: String,
+        #[serde(default = "default_u16::<8081>")]
+        http_port: u16,
+        #[serde(default = "default_u16::<8082>")]
+        grpc_port: u16,
+        #[serde(default = "default_u16::<10025>")]
+        healthz_port: u16,
+        #[serde(default = "default_u16::<9199>")]
+        metrics_port: u16
+    }
+
+    #[derive(serde::Deserialize,Debug)]
+    pub struct Db {
+        mysql: Mysql
+    }
+
+    #[derive(serde::Deserialize,Debug)]
+    pub struct Mysql {
+        #[serde(default = "default_str::<\"http://127.0.0.1\">")]
+        host: String,
+        #[serde(default = "default_u16::<3306>")]
+        port: u16,
+        #[serde(default = "default_str::<\"http://127.0.0.1\">")]
+        dating: String,
+        #[serde(default = "default_str::<\"http://127.0.0.1\">")]
+        user: String,
+        #[serde(default = "default_str::<\"http://127.0.0.1\">")]
+        pass: String,
+        connections: Connections
+    }
+
+    #[derive(serde::Deserialize,Debug)]
+    pub struct Connections {
+        #[serde(default = "default_usize::<30>")]
+        max_idle: usize,
+        #[serde(default = "default_usize::<30>")]
+        max_open: usize,
+    }
+
+    #[derive(serde::Deserialize,Debug)]
+    pub struct Log {
+        app: App
+    }
+
+    #[derive(serde::Deserialize,Debug)]
+    pub struct App {
+        #[serde(default = "default_str::<\"info\">")]
+        level: String
+    }
+
+    #[derive(serde::Deserialize,Debug)]
+    pub struct Background {
+        watchdog: Watchdog
+    }
+
+    #[derive(serde::Deserialize,Debug)]
+    pub struct Watchdog {
+        #[serde(default = "default_str::<\"5s\">")]
+        period: String,
+        #[serde(default = "default_usize::<10>")]
+        limit: usize,
+        #[serde(default = "default_str::<\"4s\">")]
+        lock_timeout: String,
+    }
 }
 
 fn main() {
     let args = Opts::parse();
 
     let builder = Config::builder()
-        .add_source(File::new("config.toml", FileFormat::Toml))
-        .add_source(Environment::with_convert_case(config::Case::UpperSnake).prefix("CONF_"))
-        .set_default("conf_file", args.conf.as_deref().unwrap_or("config.toml"))
-        .unwrap()
-        .set_default("name", "Joe Biden")
-        .unwrap();
+        .add_source(File::new(args.conf.as_deref().unwrap_or("config.toml"), FileFormat::Toml))
+        .add_source(Environment::with_convert_case(config::Case::UpperSnake).prefix("CONF_"));
 
-    let conf: Conf = builder.build().unwrap().try_deserialize().unwrap();
+    let conf: conf::Conf = builder.build().unwrap().try_deserialize().unwrap();
 
-    println!("Hello {}! the config is: {}", &conf.name, &conf.conf_file);
+    println!("Hello! the config is: {:?}", &conf);
 }
