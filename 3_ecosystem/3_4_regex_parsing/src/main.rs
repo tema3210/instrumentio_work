83fa6d43f49c20f0b02fe3c75bc84925b936d04a
tema2 <work.aab.25122001@gmail.com>
2024-03-05 14:00:03 +0100

    fmt

--- 3_ecosystem/3_4_regex_parsing/src/main.rs
+++ 3_ecosystem/3_4_regex_parsing/src/main.rs
@@ -2,8 +2,8 @@
 #![feature(if_let_guard)]
 #![feature(let_chains)]
 use pest::Parser;
-use regex::RegexBuilder;
 use pest_derive::Parser;
+use regex::RegexBuilder;
 
 fn main() {
     println!("Implement me!");
@@ -25,14 +25,14 @@ pub enum Precision {
 
 type ToGet = (Option<Sign>, Option<usize>, Option<Precision>);
 
-const NOMATCH: ToGet = (None,None,None);
+const NOMATCH: ToGet = (None, None, None);
 
 #[derive(Parser)]
 #[grammar = "grammars/fmt.pest"]
 struct FmtParser;
 
 pub fn parse_hand_version(input: &str) -> ToGet {
-    match FmtParser::parse(Rule::format_spec,input) {
+    match FmtParser::parse(Rule::format_spec, input) {
         Ok(pairs) => {
             let mut sign = None;
             let mut width = None;
@@ -42,117 +42,102 @@ pub fn parse_hand_version(input: &str) -> ToGet {
 
             for sub in pair.into_inner() {
                 match sub.as_rule() {
-                    Rule::sign => {
-                        match sub.as_str() {
-                            "+" => sign = Some(Sign::Plus),
-                            "-" => sign = Some(Sign::Minus),
-                            _ => continue
-                        }
+                    Rule::sign => match sub.as_str() {
+                        "+" => sign = Some(Sign::Plus),
+                        "-" => sign = Some(Sign::Minus),
+                        _ => continue,
                     },
                     Rule::width => {
                         match sub.as_str() {
                             i if let Ok(i) = i.parse::<usize>() => {
                                 width = Some(i);
-                            },
+                            }
                             // we are not supposed to parse parameters here
-                            _ => continue
+                            _ => continue,
                         }
-                    },
-                    Rule::precision => {
-                        match sub.as_str() {
-                            a if let Ok(arg) = a[1..].parse()
-                                && a.starts_with('$') =>
-                            {
-                                println!("the precision: {}", sub.as_str());
-                                precision = Some(Precision::Argument(arg))
-                            },
-                            a if a.starts_with('$') => {
-                                precision = Some(Precision::ArgumentStr(a[1..].into()))
-                            },
-                            i if let Ok(uint) = i.parse::<usize>() => precision = Some(Precision::Integer(uint)),
-                            "*" => {
-                                precision = Some(Precision::Asterisk)
-                            },
-                            _ => unreachable!("This")
+                    }
+                    Rule::precision => match sub.as_str() {
+                        a if let Ok(arg) = a[1..].parse()
+                            && a.starts_with('$') =>
+                        {
+                            println!("the precision: {}", sub.as_str());
+                            precision = Some(Precision::Argument(arg))
                         }
+                        a if a.starts_with('$') => {
+                            precision = Some(Precision::ArgumentStr(a[1..].into()))
+                        }
+                        i if let Ok(uint) = i.parse::<usize>() => {
+                            precision = Some(Precision::Integer(uint))
+                        }
+                        "*" => precision = Some(Precision::Asterisk),
+                        _ => unreachable!("This"),
                     },
                     _ => continue,
                 }
-            };
-            (sign,width,precision) 
-        },
+            }
+            (sign, width, precision)
+        }
         Err(_) => NOMATCH,
     }
 }
 
 const REGEX_SIGN: &str = r#"([\+-])"#;
-    
+
 const REGEX_WIDTH: &str = r#"([0-9]+)"#;
 
 const REGEX_PRECISION: &str = r#"(\.([0-9]+|\*|\$\w))"#;
 
-fn matcher(regex: &str) -> impl Fn(&str,usize) -> Option<(&str,usize)> {
+fn matcher(regex: &str) -> impl Fn(&str, usize) -> Option<(&str, usize)> {
     let the_regex = RegexBuilder::new(regex).build().unwrap(); //should have this in lazy static
 
-    move |input,key: usize| {
+    move |input, key: usize| {
         let captures = the_regex.captures(input)?;
 
         let res = captures.get(key).map(|m| m.as_str());
 
-        Some((res?,captures.len()))
+        Some((res?, captures.len()))
     }
 }
 
 /// We take in: format_spec := [[fill]align][sign]['#']['0'][width]['.' precision]type
 /// We give back: sign,width,precision
 pub fn parse(input: &str) -> ToGet {
-
     let sign = {
         let c = matcher(REGEX_SIGN);
 
-        c(input,1).and_then(
-            |(c,_)| match c {
-                "+" => Some(Sign::Plus),
-                "-" => Some(Sign::Minus),
-                _ => None
-            }
-        )
+        c(input, 1).and_then(|(c, _)| match c {
+            "+" => Some(Sign::Plus),
+            "-" => Some(Sign::Minus),
+            _ => None,
+        })
     };
 
     let width_matcher = matcher(REGEX_WIDTH);
 
     let precision_matcher = matcher(REGEX_PRECISION);
 
+    let width = width_matcher(input, 1).and_then(|(c, _)| match c {
+        w if let Ok(width) = w.parse::<usize>() => Some(width),
+        "" => None,
+        _ => unreachable!(),
+    });
 
-    let width = width_matcher(input,1).and_then(
-        |(c,_)| match c {
-            w if let Ok(width) = w.parse::<usize>() => {
-                Some(width)
-            },
-            "" => None,
-            _ => unreachable!(),
+    let precision = precision_matcher(input, 2).and_then(|(c, _)| match c {
+        "*" => Some(Precision::Asterisk),
+        i if let Ok(uint) = i.parse::<usize>() => {
+            // what if we have two integers...
+            Some(Precision::Integer(uint))
         }
-    );
-
-    let precision = precision_matcher(input,2).and_then(
-        |(c,_)| match c {
-            "*" => Some(Precision::Asterisk),
-            i if let Ok(uint) = i.parse::<usize>() => {
-                // what if we have two integers...
-                Some(Precision::Integer(uint))
-            },
-            a if a.starts_with('$') && let Ok(arg) = a[1..].parse() => {
-                Some(Precision::Argument(arg))
-            },
-            a if a.starts_with('$') => {
-                Some(Precision::ArgumentStr(a[1..].into()))
-            },
-            _ => None,
+        a if a.starts_with('$')
+            && let Ok(arg) = a[1..].parse() =>
+        {
+            Some(Precision::Argument(arg))
         }
-    );
+        a if a.starts_with('$') => Some(Precision::ArgumentStr(a[1..].into())),
+        _ => None,
+    });
 
-    (sign,width,precision)
-
+    (sign, width, precision)
 }
 
 #[cfg(test)]
